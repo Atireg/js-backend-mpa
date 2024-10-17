ded883365ae5ea6e3e8c762e97f7e2e596f13690
Atireg <134270279+Atireg@users.noreply.github.com>
2024-10-17 11:14:43 +0200

    Setup Express
    Add modular routes (routes.js file + add home controller)

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import routes from './routes.js';
 
 const app = express();
 
@@ -6,8 +7,12 @@ app.use('/static', express.static('public'));
 
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-    res.send('It works!')
-});
+
+// Initial test
+// app.get('/', (req, res) => {
+//     res.send('It works! It works!!!!!')
+// });
+
+app.use(routes);
 
 app.listen(3000, () => console.log('Server is running on http://localhost:3000'));
